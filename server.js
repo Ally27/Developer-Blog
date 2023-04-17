bdeefe52df17208a89933a0883651261dc79e11e
Allyson Gonzales <Allysonmg21@gmail.com>
2023-04-17 05:15:02 -0700

    reformated server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+const path = require('path');
+
 const express = require("express");
 const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
 const session = require("express-session");
 const helpers = require('./utils/helpers');
-const path = require('path');
 const routes = require('./controllers');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -19,7 +22,7 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
-  }),
+  })
 };
 
 app.use(session(sess));
