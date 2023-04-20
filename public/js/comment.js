66ba58731d9dd7d34bea9733221743daef03c80d
Allyson Gonzales <Allysonmg21@gmail.com>
2023-04-20 01:29:48 -0700

    created commentSubmit in comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,5 +1,26 @@
 const commentSubmit = async ( event ) =>{
     event.preventDefault();
 
-    const content = document.querySelector("#")
-}
+    const content = document.querySelector(".commentbox").value.trim();
+    const commentPost = document.querySelector(".commentbtn").value.trim();
+
+    if (content){
+        const response = await fetch("/api/post/comment",{
+            method: "POST",
+            body: JSON.stringify({
+                content,
+                commentPost
+            }),
+            headers:{"Content-Type": "application/json"},
+        });
+        if (response.ok){
+            location.reload();
+        } else {
+            alert("No input entered, please try again.");
+        }
+    }
+
+};
+
+document
+querySelector(".")
